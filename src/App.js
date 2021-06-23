d73ebe80a65b34f2bab3c410013e13d69ae62a60
stevejd88 <stevejd88@gmail.com>
2021-06-23 09:10:03 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import './App.css';
-import { useState } from 'react';
-import { Form } from './components/Form';
-import PrimaryButton from './elements/formElements/PrimaryButton';
+import "./App.css";
+import { useState } from "react";
+import { Form } from "./components/Form";
+import PrimaryButton from "./elements/formElements/PrimaryButton";
 
 const APP_STYLE = {
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  flexDirection: 'column',
-  paddingTop: '2em',
-  paddingBottom: '2em',
-  backgroundColor: '#F0EBF8',
-  minWidth: '100vw',
-  minHeight: '100vh',
-}
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  flexDirection: "column",
+  paddingTop: "2em",
+  paddingBottom: "2em",
+  backgroundColor: "#F0EBF8",
+  minWidth: "100vw",
+  minHeight: "100vh"
+};
 
 function App() {
   return (
     <div style={APP_STYLE}>
+      <h1>hello world</h1>
       <Form formName='Test Form' />
-      <div style={{ paddingTop: '1em', width: '50%', minWinth: '20em', display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
-        <PrimaryButton>
-          Submit
-        </PrimaryButton>
+      <div
+        style={{
+          paddingTop: "1em",
+          width: "50%",
+          minWinth: "20em",
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "flex-start"
+        }}
+      >
+        <PrimaryButton>Submit</PrimaryButton>
       </div>
     </div>
   );
